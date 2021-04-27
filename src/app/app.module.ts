9872856c5151c0872da20cd2a55c4391e3c0890c
ggojowy <grzegorz.gojowy@gmail.com>
2021-04-27 17:50:07 +0200

    komponent main page routowanie

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { NameComponent } from './name/name.component';
+import { CapitalcityComponent } from './capitalcity/capitalcity.component';
+import { CurrencyComponent } from './currency/currency.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    NameComponent,
+    CapitalcityComponent,
+    CurrencyComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/capitalcity/capitalcity.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-capitalcity',
+  templateUrl: './capitalcity.component.html',
+  styleUrls: ['./capitalcity.component.css']
+})
+export class CapitalcityComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
