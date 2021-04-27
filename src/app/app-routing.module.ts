f3ed68d0f2b84a69473027c67d5bdc77c80a4e07
ggojowy <grzegorz.gojowy@gmail.com>
2021-04-27 18:28:26 +0200

    api http i klawisze

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {MainPageComponent} from './main-page/main-page.component';
+import {NameComponent} from './name/name.component';
+import {CapitalcityComponent} from './capitalcity/capitalcity.component';
+import {CurrencyComponent} from './currency/currency.component';
 
 const routes: Routes = [
-  {path: '', component: MainPageComponent}
+  {path: '', component: MainPageComponent},
+  {path: 'name', component: NameComponent},
+  {path: 'capitalcity', component: CapitalcityComponent},
+  {path: 'currency', component: CurrencyComponent}
 ];
 
 @NgModule({
